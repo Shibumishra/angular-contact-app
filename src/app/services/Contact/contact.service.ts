2f7fabe684a1098519bd259c9058156565829f62
Shibumishra <sibumishra487@gmail.com>
2022-09-15 22:41:49 +0530

    services

--- src/app/services/Contact/contact.service.ts
+++ src/app/services/Contact/contact.service.ts
@@ -11,7 +11,7 @@ export class ContactService {
   constructor(private httpClient: HttpClient) { }
 
   public getAllContact(): Observable<Contact[]> {
-    let dataURL: string = '';
+    let dataURL: string = 'https://gist.githubusercontent.com/thenaveensaggam/fe5a67531d964f8c17fc92f0c70ced21/raw/f7c43379ec32421c871a57f832e314dd8e5d92f7/contacts.15.6.2021.json';
     return this.httpClient.get<Contact[]>(dataURL)
       .pipe(
         catchError(this.handleError)
